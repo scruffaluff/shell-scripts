01ae60ba2f5802e2888571a0d8b57ab3206e5639
Macklan Weinstein <macklan.weinstein@gmail.com>
2023-06-18 13:52:16 -0700

    :construction_worker: Add SSH debugging to CI jobs

--- scripts/shfmt.js
+++ scripts/shfmt.js
@@ -1,27 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * Convenience script for running Shfmt.
  */
 
-const childProcess = require('child_process');
+const childProcess = require("child_process");
 
 function main() {
   let options;
-  let paths = 'install.sh src/';
+  let paths = "install.sh src/";
 
   switch (process.argv[2]) {
-    case 'format':
-      options = '-w';
+    case "format":
+      options = "-w";
       break;
-    case 'test':
-      options = '-d';
+    case "test":
+      options = "-d";
       break;
     default:
       console.error(`Not a command: ${format}.`);
   }
 
-  childProcess.execSync(`shfmt ${options} ${paths}`, { stdio: 'inherit' });
+  childProcess.execSync(`shfmt ${options} ${paths}`, { stdio: "inherit" });
 }
 
 main();
